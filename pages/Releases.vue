ce15aa4c8a89caba78e98f4b02983dedeba677ed
Rodrigo Roigues <rs9844660@gmail.com>
2024-11-27 20:01:38 -0300

    feat: create Releases page with sidebar filters and products grid

---
+++ pages/Releases.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="releases">
+        <ResultsHeader />
+        <div class="releases__content">
+            <SidebarFilters v-show="showFilters"/>
+            <ProductsGrid />
+        </div>
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+    layout: 'custom',
+});
+
+const { showFilters } = useFilters();
+
+</script>
+
+
+<style lang="scss">
+.releases {
+    &__content {
+        display: flex;
+        transition: all ease 0.5s;
+    }
+}
+</style>
