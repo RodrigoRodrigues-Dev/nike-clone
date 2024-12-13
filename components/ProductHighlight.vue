58ebdebd32af5696243f7436d6349c196adb13b7
Rodrigo Roigues <rs9844660@gmail.com>
2024-12-12 22:30:05 -0300

    feat: create product highlight component to display highlight items - Implemented component to display product highlight items with text and images. - Styled the highlight section with responsive adjustments for different screen sizes.

---
+++ components/ProductHighlight.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="product-highlight">
+        <div 
+            class="product-highlight__content" 
+            v-for="(item, index) in productStore.highlightItems" 
+            :key="index"
+        >
+            <h2 class="product-highlight__text">{{ item.text }}</h2>
+            <img class="product-highlight__image" :src="item.image" alt="">
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import { useProductStore } from '~/stores/productStore';
+
+    const productStore = useProductStore();
+</script>
+
+<style lang="scss">
+    .product-highlight {
+        margin: 0 auto;
+        width: 90%;
+
+        &__image {
+            width: 100%;
+        }
+
+        &__text {
+            margin: 5rem auto;
+            text-align: center;
+            font-size: 1.35rem;
+            font-weight: 100;
+
+            @media (max-width: 960px) {
+                margin: 3rem 0;
+                font-size: 1rem;
+            }
+        }
+    }
+</style>
