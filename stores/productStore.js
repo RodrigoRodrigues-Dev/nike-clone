d9fcafb44c98a8248493d8224834bc27f4beb4a4
Rodrigo Roigues <rs9844660@gmail.com>
2024-12-12 22:28:05 -0300

    feat: create Pinia store 'useProductStore' for managing product data - Added state properties for product details: price, images, description, sizes, etc. - Created 'setProductProperties' action to populate product data in the store. - Enabled persistence of store state using Pinia PersistedState plugin.

---
+++ stores/productStore.js
@@ -0,0 +1,39 @@
+import { defineStore } from 'pinia';
+
+export const useProductStore = defineStore('product', {
+  state: () => ({
+    index: [],
+    price: null,
+    mainImage: null,
+    subTitle: null,
+    discountLabel: null,
+    ecoFriendlyLabel: null,
+    additionalImages: null,
+    name: null,
+    sizes: null,
+    description: null,
+    colorDescription: null,
+    styleCode: null,
+    highlightItems: null,
+    selectedGallery: [],
+  }),
+  actions: {
+    setProductProperties(prop) {
+      this.price = prop.priceUSD || null;
+      this.mainImage = prop.images.additional.map(image => image.thumbnail) || null;
+      this.subTitle = prop.productDescription || null;
+      this.discountLabel = prop.promoLabel || null;
+      this.ecoFriendlyLabel = prop.ecoFriendlyLabel || null;
+      this.additionalImages = prop.images.additional.map(image => image.thumbnail) || null;
+      this.name = prop.productName || null;
+      this.sizes = prop.details.sizes || null;
+      this.description = prop.description || null;
+      this.colorDescription = prop.details.colorDescription || null;
+      this.styleCode = prop.details.styleCode || null;
+      this.selectedGallery = prop.images.additional.map(image => image.gallery) || [];
+      this.index = prop.productId || [];
+      this.highlightItems = prop.details.highlight || [];
+    },
+  },
+  persist: true,
+});
