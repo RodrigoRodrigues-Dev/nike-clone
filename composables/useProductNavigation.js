64b9bfe30257bc72a96de92d5caeefeacbaf36f2
Rodrigo Roigues <rs9844660@gmail.com>
2024-12-20 21:44:22 -0300

    refactor: move product navigation logic to useProductNavigation composable

---
+++ composables/useProductNavigation.js
@@ -0,0 +1,17 @@
+import { useRouter } from 'vue-router';
+import { useProductStore } from '~/stores/productStore';
+
+export function useProductNavigation() {
+  const router = useRouter();
+  const productStore = useProductStore();
+
+  const goToProduct = (item) => {
+    productStore.setProductProperties(item);
+    router.push({
+      name: 'Releases-name',
+      params: { name: item.productName },
+    });
+  };
+
+  return { goToProduct };
+}
