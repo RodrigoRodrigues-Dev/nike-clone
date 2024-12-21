5b943d8dfe3459185b441f896d18c85d4e3a8daf
Rodrigo Roigues <rs9844660@gmail.com>
2024-12-20 21:43:37 -0300

    feat: move API call to App.vue to load products globally

--- app.vue
+++ app.vue
@@ -3,3 +3,24 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup>
+import axios from 'axios';
+import { useFiltersStore } from './stores/filtersStore';
+import { useSearchResults } from '~/stores/searchResults';
+
+const filtersStore = useFiltersStore();
+const searchResults = useSearchResults();
+
+onMounted(async () => {
+  try {
+    const { data } = await axios.get('https://cf53ba9b308f2696.mokky.dev/products');
+    setTimeout(() => {
+      filtersStore.setProducts(data);
+      searchResults.products = data;
+    }, 550);
+  } catch (error) {
+    console.error('Error fetching products:', error);
+  }
+});
+</script>
