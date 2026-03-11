ba8a6dafd498d59a04d4428911ad78745fc77157
Rodrigo Rodrigues <rodrigo.rsmail@gmail.com>
2026-03-11 14:45:50 -0300

    fix(products-grid): correct filtersStore import path and adjust .products-grid layout styles

--- components/layout/ProductsGrid.vue
+++ components/layout/ProductsGrid.vue
@@ -1,32 +1,27 @@
 <template>
-  <div v-if="!visibleProducts.length">
-    <div class="no-results">
-      <p>No products found.</p>
-    </div>
+  <div v-if="!visibleProducts.length" class="no-results">
+    <p>No products found.</p>
   </div>
-  <div v-else>
-    <div class="products-grid">
-      <UiProductsCard
-        v-for="item in visibleProducts"
-        :key="item.productId"
-        :mainImage="item.images.main"
-        :title="item.productName"
-        :message="item.labels.ecoFriendly"
-        :subtitle="item.productDescription"
-        :additional-images="item.images.thumbnailImages"
-        :colorOptions="`colors ${item.details.availableColors.length}`"
-        :price="`$${item.priceUSD}`"
-        :promotion="item.discountLabel"
-        :promo="item.promoLabel"
-        @click="goToProduct(item)"
-      />
-    </div>
+  <div v-else class="products-grid">
+    <UiProductsCard 
+      v-for="item in visibleProducts" 
+      :key="item.productId" 
+      :mainImage="item.images.main"
+      :title="item.productName" 
+      :message="item.labels.ecoFriendly" 
+      :subtitle="item.productDescription"
+      :additional-images="item.images.thumbnailImages" 
+      :colorOptions="`colors ${item.details.availableColors.length}`"
+      :price="`$${item.priceUSD}`" 
+      :promotion="item.discountLabel" 
+      :promo="item.promoLabel"
+      @click="goToProduct(item)" />
   </div>
 </template>
 
 <script setup>
 import { computed } from 'vue';
-import { useFiltersStore } from './stores/filtersStore';
+import { useFiltersStore } from '../../stores/filtersStore';
 import { useProductNavigation } from '~/composables/useProductNavigation';
 
 const filtersStore = useFiltersStore();
@@ -37,17 +32,21 @@ const visibleProducts = computed(() => filtersStore.filteredProducts);
 
 <style lang="scss">
 .products-grid {
-  margin: 0 2rem;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   row-gap: 3.5rem;
   column-gap: 1.5rem;
 
+  margin: 0 2rem;
+  flex: 1;
+  width: 100%;
+
   @media (max-width: 960px) {
-    margin: 0 1rem;
     grid-template-columns: repeat(2, 1fr);
     row-gap: 2rem;
     column-gap: 0.5rem;
+
+    margin: 0 1rem;
   }
 }
 
